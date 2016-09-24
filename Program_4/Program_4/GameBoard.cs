e48e468754d13657c62990fdfb77a10cef341c32
yellowahra <ahracho216@gmail.com>
2016-09-24 16:41:25 -0500

    images

--- Program_4/Program_4/GameBoard.cs
+++ Program_4/Program_4/GameBoard.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Program_4
 {
-    public partial class Form1 : Form
+    public partial class GameBoard : Form
     {
-        public Form1()
+        public GameBoard()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox19_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
