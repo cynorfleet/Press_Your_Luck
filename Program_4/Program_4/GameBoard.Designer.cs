5fc88b2ac1a39c643df3c60d974c24af95696107
yellowahra <ahracho216@gmail.com>
2016-10-05 10:53:17 -0500

    questionanswer class

--- Program_4/Program_4/GameBoard.Designer.cs
+++ Program_4/Program_4/GameBoard.Designer.cs
@@ -32,18 +32,17 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameBoard));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.answerBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.helpButton = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label7 = new System.Windows.Forms.Label();
+            this.player1spins = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.scoreP1 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label11 = new System.Windows.Forms.Label();
+            this.player2spins = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
@@ -68,6 +67,11 @@
             this.TileBox = new System.Windows.Forms.GroupBox();
             this.SpinButton = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.getSpinbutton = new System.Windows.Forms.Button();
+            this.qtextbox = new System.Windows.Forms.RichTextBox();
+            this.submitbutton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.cwlabel = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -116,14 +120,14 @@
             this.button2.Text = "STOP";
             this.button2.UseVisualStyleBackColor = false;
             // 
-            // textBox2
+            // answerBox
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(152, 475);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(472, 38);
-            this.textBox2.TabIndex = 23;
-            this.textBox2.Text = "textBox2";
+            this.answerBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answerBox.Location = new System.Drawing.Point(152, 475);
+            this.answerBox.Name = "answerBox";
+            this.answerBox.Size = new System.Drawing.Size(299, 30);
+            this.answerBox.TabIndex = 23;
+            this.answerBox.Text = "Please type your Answer!";
             // 
             // label1
             // 
@@ -145,15 +149,6 @@
             this.label2.TabIndex = 25;
             this.label2.Text = "Answer";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(247, 439);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(118, 13);
-            this.label3.TabIndex = 26;
-            this.label3.Text = "Show Questions(label3)";
-            // 
             // helpButton
             // 
             this.helpButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -167,7 +162,7 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.player1spins);
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.scoreP1);
             this.groupBox1.Controls.Add(this.label4);
@@ -178,14 +173,14 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Player1";
             // 
-            // label7
+            // player1spins
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(66, 69);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(73, 13);
-            this.label7.TabIndex = 3;
-            this.label7.Text = "label7(spinP1)";
+            this.player1spins.AutoSize = true;
+            this.player1spins.Location = new System.Drawing.Point(66, 69);
+            this.player1spins.Name = "player1spins";
+            this.player1spins.Size = new System.Drawing.Size(0, 13);
+            this.player1spins.TabIndex = 3;
+            this.player1spins.Click += new System.EventHandler(this.label7_Click);
             // 
             // label6
             // 
@@ -216,7 +211,7 @@
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.player2spins);
             this.groupBox2.Controls.Add(this.label10);
             this.groupBox2.Controls.Add(this.label9);
             this.groupBox2.Controls.Add(this.label8);
@@ -227,14 +222,13 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Player2";
             // 
-            // label11
+            // player2spins
             // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(57, 65);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(79, 13);
-            this.label11.TabIndex = 3;
-            this.label11.Text = "label11(spinP2)";
+            this.player2spins.AutoSize = true;
+            this.player2spins.Location = new System.Drawing.Point(57, 65);
+            this.player2spins.Name = "player2spins";
+            this.player2spins.Size = new System.Drawing.Size(0, 13);
+            this.player2spins.TabIndex = 3;
             // 
             // label10
             // 
@@ -466,19 +460,71 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // getSpinbutton
+            // 
+            this.getSpinbutton.Location = new System.Drawing.Point(651, 252);
+            this.getSpinbutton.Name = "getSpinbutton";
+            this.getSpinbutton.Size = new System.Drawing.Size(75, 23);
+            this.getSpinbutton.TabIndex = 34;
+            this.getSpinbutton.Text = "Get Spins";
+            this.getSpinbutton.UseVisualStyleBackColor = true;
+            this.getSpinbutton.Click += new System.EventHandler(this.getSpinbutton_Click);
+            // 
+            // qtextbox
+            // 
+            this.qtextbox.Location = new System.Drawing.Point(150, 428);
+            this.qtextbox.Name = "qtextbox";
+            this.qtextbox.Size = new System.Drawing.Size(301, 41);
+            this.qtextbox.TabIndex = 35;
+            this.qtextbox.Text = "";
+            this.qtextbox.TextChanged += new System.EventHandler(this.qtextbox_TextChanged);
+            // 
+            // submitbutton
+            // 
+            this.submitbutton.Location = new System.Drawing.Point(457, 478);
+            this.submitbutton.Name = "submitbutton";
+            this.submitbutton.Size = new System.Drawing.Size(91, 31);
+            this.submitbutton.TabIndex = 36;
+            this.submitbutton.Text = "submit";
+            this.submitbutton.UseVisualStyleBackColor = true;
+            this.submitbutton.Click += new System.EventHandler(this.submitbutton_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(554, 479);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(74, 30);
+            this.nextButton.TabIndex = 37;
+            this.nextButton.Text = "next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // cwlabel
+            // 
+            this.cwlabel.AutoSize = true;
+            this.cwlabel.Location = new System.Drawing.Point(472, 428);
+            this.cwlabel.Name = "cwlabel";
+            this.cwlabel.Size = new System.Drawing.Size(35, 13);
+            this.cwlabel.TabIndex = 38;
+            this.cwlabel.Text = "label3";
+            // 
             // GameBoard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(787, 518);
+            this.Controls.Add(this.cwlabel);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.submitbutton);
+            this.Controls.Add(this.qtextbox);
+            this.Controls.Add(this.getSpinbutton);
             this.Controls.Add(this.SpinButton);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.helpButton);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.answerBox);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.TileBox);
@@ -516,18 +562,17 @@
         #endregion
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox answerBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button helpButton;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label player1spins;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label scoreP1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label player2spins;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label8;
@@ -552,6 +597,11 @@
         private System.Windows.Forms.GroupBox TileBox;
         private System.Windows.Forms.Button SpinButton;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button getSpinbutton;
+        private System.Windows.Forms.RichTextBox qtextbox;
+        private System.Windows.Forms.Button submitbutton;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Label cwlabel;
     }
 }
 
